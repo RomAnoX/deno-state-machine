a5808c32b5b7bb712edbcca15a5913b3b3c87d99
Ulises Acosta <ulises.xsoft@gmail.com>
2020-05-27 16:45:58 -0700

    Merge interfaces into main file

--- state-machine.ts
+++ state-machine.ts
@@ -1,4 +1,37 @@
-import "./interfaces.ts";
+interface IActionState {
+  to: string;
+  before?: IActionEvent;
+  after?: IActionEvent;
+}
+
+interface IActionStates {
+  [from: string]: IActionState;
+}
+
+interface IActions {
+  [action: string]: IActionStates;
+}
+
+interface IActionEvent {
+  (args: object, data: object): void | boolean | string;
+}
+
+interface IStateAction {
+  action: string;
+  to: string;
+  before?: IActionEvent;
+  after?: IActionEvent;
+}
+
+interface IStates {
+  [state: string]: IStateAction[];
+}
+
+interface IConfig {
+  states: IStates;
+  initial: string;
+  data?: object;
+}
 
 class StateMachine {
   _config: IConfig;
